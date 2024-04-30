525c4227d0e62063651545b7e089431e0751e8f0
Shahid Nauman <shahidnauman@live.com>
2024-04-30 20:15:36 +0500

    Resoled the Conflicts between react-draggable and react-resizable

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import React, { useEffect, useState } from "react";
 import ESignPDF from "./components/esign-pdf/ESignPDF";
 import FileUploader from "./FileUploader";
 
-// const signatureImageUrl =
-//   "https://www.signwell.com/assets/vip-signatures/muhammad-ali-signature-3f9237f6fc48c3a04ba083117948e16ee7968aae521ae4ccebdfb8f22596ad22.svg";
-// const signatureImageUrl =
-//   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXQ4S4XkfGN-JDeqJqhzRCY0GqraBNfVup9wKozZKl4Q&s";
 const signatureImageUrl = require("./assets/images/signature.png");
 
 function App() {
--- src/components/esign-pdf/Signature.tsx
+++ src/components/esign-pdf/Signature.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent } from "react";
+import React, { SyntheticEvent, useEffect, useRef, useState } from "react";
 import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
 import { Resizable, ResizeCallbackData } from "react-resizable";
 import "react-resizable/css/styles.css";
@@ -11,38 +11,123 @@ type SignatureProps = {
 };
 
 function Signature({ signature, position, onPositionChange }: SignatureProps) {
-  function handleMove(_e: DraggableEvent, data: DraggableData): false | void {
-    onPositionChange?.({
-      ...position,
-      translateX: data.lastX,
-      translateY: data.lastY,
-    });
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [isResizing, setIsResizing] = useState<boolean>(false);
+
+  const signatureRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClick(e: MouseEvent) {
+      if (
+        signatureRef.current &&
+        !signatureRef.current.contains(e.target as Node)
+      ) {
+        setIsSelected(false);
+      }
+    }
+
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  function handleDragMouseDown(_e: MouseEvent): void {
+    // console.log("Drag Mouse Downed! isResizing:", isResizing);
+    if (!isResizing) {
+      setIsSelected(true);
+      setIsDragging(true);
+    }
+  }
+
+  // function handleDragStart(
+  //   e: DraggableEvent,
+  //   data: DraggableData
+  // ): false | void {
+  //   console.log("Drag Started! isResizing:", isResizing);
+  //   if (!isResizing) {
+  //   }
+  // }
+
+  // function handleDrag(e: DraggableEvent, data: DraggableData): false | void {
+  //   console.log("Dragging... isResizing:", isResizing);
+  //   if (!isResizing) {
+  //   }
+  // }
+
+  function handleDragStop(_e: DraggableEvent, { x, y }: DraggableData) {
+    // console.log("Drag Stopped! isResizing:", isResizing);
+    if (!isResizing) {
+      onPositionChange?.({ ...position, left: x, top: y });
+      setIsDragging(false);
+    }
+  }
+
+  function handleResizeStart(
+    e: SyntheticEvent<Element, Event>,
+    _data: ResizeCallbackData
+  ) {
+    // console.warn("Resize Started! isDragging:", isDragging);
+    if (!isDragging) {
+      e.stopPropagation();
+      setIsResizing(true);
+    }
   }
 
   function handleResize(
     _e: SyntheticEvent<Element, Event>,
-    data: ResizeCallbackData
+    { size }: ResizeCallbackData
   ) {
-    onPositionChange?.({
-      ...position,
-      width: data.size.width,
-      height: data.size.height,
-    });
+    // console.warn("Resizing... isDragging:", isDragging);
+    if (!isDragging) {
+      onPositionChange?.({
+        ...position,
+        width: size.width,
+        height: size.height,
+      });
+    }
+  }
+
+  function handleResizeStop(
+    _e: SyntheticEvent<Element, Event>,
+    _data: ResizeCallbackData
+  ) {
+    // console.warn("Resize Stoped! isDragging:", isDragging);
+    if (!isDragging) {
+      setIsResizing(false);
+    }
   }
 
   return (
-    <Draggable onStop={handleMove}>
+    <Draggable
+      position={{ x: position.left, y: position.top }}
+      onMouseDown={handleDragMouseDown}
+      // onStart={handleDragStart}
+      // onDrag={handleDrag}
+      onStop={handleDragStop}
+    >
       <Resizable
         width={position.width}
         height={position.height}
+        onResizeStart={handleResizeStart}
         onResize={handleResize}
+        onResizeStop={handleResizeStop}
       >
-        <div style={{ ...position, position: "absolute" }}>
+        <div
+          ref={signatureRef}
+          style={{
+            width: position.width + Number(isSelected) * 2,
+            height: position.height + Number(isSelected) * 2,
+            position: "absolute",
+            ...(isSelected && { border: "1px dashed #000", margin: "-1px" }),
+          }}
+        >
           <img
             src={URL.createObjectURL(signature)}
             alt=""
-            width={"100%"}
-            height={"100%"}
+            width="100%"
+            height="100%"
           />
         </div>
       </Resizable>
